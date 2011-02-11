b25b25570d71681b55ea568a1b6c506647cb0986
ibrunton <wolfshift@gmail.com>
2011-02-11 16:56:57 -0400

    Added support for RC specification of editor

--- editlog.pl
+++ editlog.pl
@@ -3,13 +3,14 @@
 use Modern::Perl;
 use Log;
 
-my $action = $ENV{EDITOR} // '/usr/bin/env vim';
-my $alternate = 'emacsclient';
-
 my $input = join( ' ', @ARGV );
 
 my $log = Log->new;
 $log->getopts( 'ajt', \$input );
+$log->parse_rc;
+
+my $action = $log->{editor} // $ENV{EDITOR} // '/usr/bin/env vim';
+my $alternate = 'emacsclient';
 
 # opt 'a' does something different from other log scripts:
 if ( $log->opt( 'a' ) ) { $action = $alternate; }
