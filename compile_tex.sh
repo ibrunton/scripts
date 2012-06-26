c91345d53b078dcfdf6f0e03d4159c4c584781e2
Ian Brunton <wolfshift@gmail.com>
2012-06-26 16:41:56 -0300

    added compile_tex.sh

---
+++ compile_tex.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# recompiles term papers and thesis chapters in latex
+# 2012 by Ian D Brunton < iandbrunton at gmail dot com >
+
+# usage: COURSE=x1/5613 compile_tex.sh
+# usage: COURSE=thesis/ch1 PAPER=draft1.tex compile_tex.sh
+
+src_dir=$HOME/Dropbox/docs/school/"$COURSE"
+paper_body=${PAPER:-paper.tex}
+
+cat $src_dir/_header.tex $src_dir/$PAPER $src_dir/_footer.tex > ./paper.tex
+rsync $src_dir/sources/*.bib ./sources/
+
+latex paper.tex
+bibtex paper
+latex paper.tex
+dvipdf paper.dvi
