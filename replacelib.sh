40f889cd596cb266de17e3de1ce3ebb85068d8f7
Ian Brunton <wolfshift@gmail.com>
2012-07-11 07:20:07 -0300

    added error check to replacelib

--- replacelib.sh
+++ replacelib.sh
@@ -4,5 +4,11 @@
 # without having to track the directory down every time.
 # 2011-12-31
 
+if [ -z $1 ]
+then
+	echo "Please specify which library you wish to copy"
+	exit
+fi
+
 lib=$1
 sudo cp $1 /usr/lib/perl5/vendor_perl/
