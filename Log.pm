305e901bf86f4b21447ce80f809ad35f39e91790
ibrunton <iandbrunton@gmail.com>
2013-06-06 09:36:58 -0300

    changed underline character to _

--- Log.pm
+++ Log.pm
@@ -533,7 +533,7 @@ sub markup {
 	$$string =~ s/^(\#.+)$/$self->comment_markup($1)/e;
 
 	# underline:
-	$$string =~ s|/([^,;]+)/|$self->underline($1)|eg;
+	$$string =~ s|_([^,;]+)_|$self->underline($1)|eg;
     	#}
     return $self;
 }
