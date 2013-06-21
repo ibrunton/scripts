d7e60771e05b6bc9b28c90029162d55f070631dc
Ian D. Brunton <iandbrunton@gmail.com>
2013-06-21 15:20:17 -0300

    added cofn, added stat to subjects.pl

--- subjects.pl
+++ subjects.pl
@@ -5,31 +5,39 @@
 
 use Modern::Perl;
 use XML::Simple;
+use IDB;
 
 my $home = $ENV{HOME};
 
 my $username = $ARGV[0];
+my $filename = "$home/.local/share/inbox_$username";
 
 my $xml = new XML::Simple;
-my $inbox = $xml->XMLin ("$home/.local/share/inbox_$username");
+my $inbox = $xml->XMLin ($filename);
+my @amodtime = localtime ((stat ($filename))[9]);
+my $modtime = join (' ', join ('-', $amodtime[5] + 1900,
+	&IDB::double_digit ($amodtime[4]), &IDB::double_digit ($amodtime[3])),
+    ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')[$amodtime[6]],
+    join (':', &IDB::double_digit ($amodtime[2]), &IDB::double_digit ($amodtime[1])));
 
-$inbox->{modified} =~ s/[TZ]/ /g;
+#$inbox->{modified} =~ s/[TZ]/ /g;
 
 print "Messages for $username: ", $inbox->{fullcount}, "\n";
-print "at ", $inbox->{modified}, "\n\n";
+#print "at ", $inbox->{modified}, "\n\n";
+print "$modtime\n\n";
 
 if ($inbox->{fullcount} > 0) {
 	if ($inbox->{fullcount} > 1) {
 		foreach my $msg (keys %{$inbox->{entry}}) {
-			print "From: ", $inbox->{entry}->{$msg}->{author}->{name}, " ";
+			print "  From: ", $inbox->{entry}->{$msg}->{author}->{name}, " ";
 			print "<", $inbox->{entry}->{$msg}->{author}->{email}, ">\n";
-			print "Subject: ", $inbox->{entry}->{$msg}->{title}, "\n\n";
+			print "  Subject: ", $inbox->{entry}->{$msg}->{title}, "\n\n";
 		}
 	}
 	else {
-		print "From: ", $inbox->{entry}->{author}->{name}, " ";
+		print "  From: ", $inbox->{entry}->{author}->{name}, " ";
 		print "<", $inbox->{entry}->{author}->{email}, ">\n";
-		print "Subject: ", $inbox->{entry}->{title}, "\n\n";
+		print "  Subject: ", $inbox->{entry}->{title}, "\n\n";
 	}
 }
 
