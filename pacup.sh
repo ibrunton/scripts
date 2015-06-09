aa875e2f90a87eb1733915c99f98a1cfcdeb069f
Ian D. Brunton <iandbrunton@gmail.com>
2015-06-09 16:32:00 -0300

    minor tweaks

--- pacup.sh
+++ pacup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pacman -Qu > $HOME/.logs/pacman/`date "+%Y-%m-%d_%H%M"`
-#yaourt -Su --ignore firefox
+
 sudo pacman -Su
--- suspend.sh
+++ suspend.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 emacsclient -n -e "(kill-buffer \"todo.org\")"
-sudo /usr/sbin/pm-suspend
+if [ -x /usr/sbin/pm-suspend ]
+then
+	sudo /usr/sbin/pm-suspend
+else
+	sudo systemctl suspend
+fi
