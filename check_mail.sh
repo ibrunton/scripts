15eeae7fab098b05e8e08ab19381a11b322ba2fa
Ian D. Brunton <ian.d.brunton@gmail.com>
2017-09-25 16:25:55 -0300

    moved the loop to crontab

--- check_mail.sh
+++ check_mail.sh
@@ -3,10 +3,6 @@
 XDG_DATA_HOME=${XDG_DATA_HOME:-/home/ian/.local/share}
 WOLFSHIFT_FILE=$XDG_DATA_HOME/mailcount_wolfshift
 IANDBRUNTON_FILE=$XDG_DATA_HOME/mailcount_iandbrunton
-MAIL_INTERVAL=300
 
-while true ; do
-	python2 $HOME/scripts/gmail.py wolfshift > $WOLFSHIFT_FILE
-	python2 $HOME/scripts/gmail.py iandbrunton > $IANDBRUNTON_FILE
-	sleep $MAIL_INTERVAL
-done &
+python2 $HOME/scripts/gmail.py wolfshift > $WOLFSHIFT_FILE
+python2 $HOME/scripts/gmail.py iandbrunton > $IANDBRUNTON_FILE
